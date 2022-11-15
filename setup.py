579da83332299b40ee290f04fc536b86f9bf6b3d
Lucas Liu <bo.liu@iherb.com>
2022-11-15 17:27:59 +0800

    update setup

--- setup.py
+++ setup.py
@@ -14,12 +14,26 @@ setuptools.setup(
     description="page factory",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="<LINK_TO_YOUR_CODE_OR_PRODUCT>",
+    url="https://github.com/lucas234/pyium",
+    zip_safe=False,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X'
     ],
-    packages=setuptools.find_packages(),
-    python_requires=">=3.5"
+    install_requires=[
+        'selenium==3.141.0',
+        'Appium-Python-Client==2.7.1',
+    ],
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    python_requires=">=3"
 )
